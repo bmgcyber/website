8dc4fa1fe07d92657ca3109313478ddedd7de129
Lauro Guedes <leowgweb@gmail.com>
2025-12-13 19:24:07 +0100

    Add Bloomfolio portfolio template configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,23 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react(), markdoc(), keystatic()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
